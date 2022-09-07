0618b37ee5e32779a940d5e8a72334b4601f350d
ALberto Lotz <55667164+albertolotz@users.noreply.github.com>
2022-09-06 23:38:01 -0300

    Finalizado Operadores Aritméticos em C#

---
+++ Intro-dotnet/OperadoresAritimeticos/Program.cs
@@ -0,0 +1,13 @@
+﻿using OperadoresAritimeticos.Models;
+
+Calculadora calc = new Calculadora();
+
+calc.Somar(30, 60);
+calc.Subitrair(90,20);
+calc.Dividir(100, 2);
+calc.Multiplicar(200, 4);
+calc.Potencia(3,7);
+calc.Seno(67);
+calc.Coseno(44);
+calc.Tangente(89);
+calc.RaizQuadrada(4);
