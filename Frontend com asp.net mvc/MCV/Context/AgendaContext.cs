d89999ab68d990662f78621ba9c5c775185f16dc
ALberto Lotz <55667164+albertolotz@users.noreply.github.com>
2022-09-25 21:10:13 -0300

    final-MVC

---
+++ Frontend com asp.net mvc/MCV/Context/AgendaContext.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MCV.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MCV.Context
+{
+    public class AgendaContext : DbContext  
+    {
+       public AgendaContext(DbContextOptions<AgendaContext>options) : base(options)
+       {
+
+       }
+
+       public DbSet<Contato> Contatos { get; set; }
+        
+    }
+}
